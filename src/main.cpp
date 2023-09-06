e4b07dda17ee4157427796a899a6ffd34c74cb84
theMladyPan <stanislav@rubint.sk>
2023-09-06 22:02:25 +0200

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,62 @@
 #include <TFT_eSPI.h>
 #include <BMI160Gen.h>
 #include <ESP32Servo.h>
-
-Servo sx_pos;
-Servo sy_pos;
-Servo sx_neg;
-Servo sy_neg;
+#include <PID_v1.h>
+#include <math.h>
+#include <esp_log.h>
+#include <vector>
 
 TFT_eSPI tft = TFT_eSPI(135, 240);
 
 
+double Kp=1, Ki=10, Kd=0;
+
+//Define Variables we'll be connecting to
+double angle_x_set, angle_x, corr_x;
+PID pid_sx_pos(&angle_x, &corr_x, &angle_x_set, Kp, Ki, Kd, DIRECT);
+
 float convertRawGyro(int gRaw) {
     float g = (gRaw * 1000.0) / 32768.0;
-
     return g;
 }
 
 float convertRawAccel(int aRaw) {
     float a = (aRaw * 2.0) / 32768.0;
-
     return a;
+}
+
+class IServo {
+public:
+    IServo(Servo &servo) {
+        _servo = servo;
+    }
+    IServo(int pin) {
+        _servo = Servo();
+        _servo.attach(pin, 500, 2500);
+        _servo.setPeriodHertz(200);
+    }
+    void setAngle(int angle) {
+        // 500ms = -90°
+        // 1500ms = 0°
+        // 2500ms = 90°
+        // 1° = 11.11111ms
+
+        int ms = 1500 + (angle * 11.11111); 
+        _servo.writeMicroseconds(ms);
+        
+    }
+private:
+    Servo _servo;
+};
+
+void axesToVector(std::array<float, 3> &vec, float ax, float ay, float az) {
+    vec[0] = ax;
+    vec[1] = ay;
+    vec[2] = az;
+}
+
+float vectorLength(std::array<float, 3> &vec) {
+    return sqrt(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2]);
 }
 
 void setup() {
@@ -36,14 +73,11 @@ void setup() {
 	ESP32PWM::allocateTimer(1);
 	ESP32PWM::allocateTimer(2);
 	ESP32PWM::allocateTimer(3);
-    sx_pos.setPeriodHertz(200);
-    sx_pos.attach(25, 500, 2500);
-    sy_pos.setPeriodHertz(200);
-    sy_pos.attach(26, 500, 2500);
-    sx_neg.setPeriodHertz(200);
-    sx_neg.attach(27, 500, 2500);
-    sy_neg.setPeriodHertz(200);
-    sy_neg.attach(33, 500, 2500);
+
+    //turn the PID on
+    pid_sx_pos.SetMode(AUTOMATIC);
+    pid_sx_pos.SetSampleTime(1);
+    pid_sx_pos.SetOutputLimits(-90, 90);
 
     while(!Serial) { 
         delay(1);
@@ -62,7 +96,6 @@ void setup() {
     BMI160.setFullScaleGyroRange(BMI160_GYRO_RANGE_1000);
     ESP_LOGD("BMI160", "Setting accelerometer range done.");
     BMI160.setFullScaleAccelRange(BMI160_ACCEL_RANGE_2G);
-    
 }
 
 void loop() {
@@ -70,6 +103,17 @@ void loop() {
     int axRaw, ayRaw, azRaw;         // raw accelerometer values
     float gx = 0, gy = 0, gz = 0;
     float ax = 0, ay = 0, az = 0;
+    std::array<float, 3> vecAcc;
+    std::array<float, 4> quatAcc;
+
+    uint64_t iter = 0;
+
+    IServo servoXPos(33);
+    IServo servoYPos(25);
+    IServo servoXNeg(26);
+    IServo servoYNeg(27);
+    IServo throttle(32);
+
 
     while(1) {
         int64_t time_start = esp_timer_get_time();
@@ -78,19 +122,35 @@ void loop() {
         BMI160.readAccelerometer(axRaw, ayRaw, azRaw);
 
         // convert the raw accelerometer data to degrees/second
-        ax = convertRawAccel(axRaw) * 0.01 + ax * 0.99;
-        ay = convertRawAccel(ayRaw) * 0.01 + ay * 0.99;
-        az = convertRawAccel(azRaw) * 0.01 + az * 0.99;
+        ax = convertRawAccel(axRaw);
+        ay = convertRawAccel(ayRaw);
+        az = convertRawAccel(azRaw);
 
+        axesToVector(vecAcc, ax, ay, az);
+        float len = vectorLength(vecAcc);
+
+        // ESP_LOGI("BMI160", "Accel: %f, %f, %f", ax, ay, az);
         // convert the raw gyro data to degrees/second
-        gx = convertRawGyro(gxRaw) * 0.01 + gx * 0.99;
-        gy = convertRawGyro(gyRaw) * 0.01 + gy * 0.99;
-        gz = convertRawGyro(gzRaw) * 0.01 + gz * 0.99;
-        int64_t dt = esp_timer_get_time() - time_start;
-        sx_pos.writeMicroseconds(1500 + gz * 100 + ax * 1000);
+        //gx = convertRawGyro(gxRaw) * 0.01 + gx * 0.99;
+        //gy = convertRawGyro(gyRaw) * 0.01 + gy * 0.99;
+        gz = convertRawGyro(gzRaw);
+        angle_x_set = 0;
+        if(ay >= 1.0) ay = 1.0;
+        if(ay <= -1.0) ay = -1.0;
+        float asiny = asin(ay);
+        angle_x = asin(ay) * -180 / M_PI;
 
-        // display tab-separated gyro x/y/z values
-        Serial.printf("\rax: %.2f, ay: %.2f, az: %.2f, gx: %d°, \tgy: %d°, \tgz: %d°, \tdt: %lldus", ax, ay, az, (int)gx, (int)gy, (int)gz, dt);
-        //delay(10);
+        servoYPos.setAngle(corr_x);
+        
+        int64_t dt = esp_timer_get_time() - time_start;
+        if (dt < 1000) {
+            delayMicroseconds(1000 - dt);
+        }
+        pid_sx_pos.Compute();
+        if (iter++ % 100 == 0) {
+            ESP_LOGI("BMI160", "Angle: %.2f, %.2f, dt: %lldus", angle_x, corr_x, dt);
+            // print vector length
+            ESP_LOGI("BMI160", "Vector length: %f", len);
+        }
     }
 }
